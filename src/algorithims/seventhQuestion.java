19e2ddeeda96b1a866a6aaac70e4fd97c3ec8f43
Goboitumelo <goboitumelosimon85@gmail.com>
2021-08-05 15:58:18 +0200

    Last Question

--- src/algorithims/seventhQuestion.java
+++ src/algorithims/seventhQuestion.java
@@ -10,8 +10,9 @@ public class seventhQuestion {
             for(int j=1;j<=word2.length();j++){
                 if(word1.charAt(i-1)==word2.charAt(j-1)) d[i][j]=d[i-1][j-1];
                 else d[i][j]=Math.min(Math.min(d[i-1][j],d[i][j-1]),d[i-1][j-1])+1;
-                System.out.println(d[word1.length()][word2.length()]);
+
             }
+            System.out.println(d[word1.length()][word2.length()]);
         }
         return d[word1.length()][word2.length()];
     }
